bf60655df0f09c20f8c3a7364cef0d347f96c12c
sinandsean <sinandsean@gmail.com>
2022-12-17 21:59:16 +0900

    tailwind

---
+++ src/Main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <div className="fixed inset-0 bg-black">
+      <div className="bg-red-500">aaa</div>
+    </div>
+  );
+}
+
+export default Main;
